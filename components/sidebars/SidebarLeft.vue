ba7733d4e6c0382ea60e313b68aefe9d94dfd803
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-01-03 19:34:35 +0100

    Update SidebarLeft.vue

--- components/sidebars/SidebarLeft.vue
+++ components/sidebars/SidebarLeft.vue
@@ -171,6 +171,12 @@
               </li>
 
               <li class="pt-1 pb-1" @click="closeLeftSidebar">
+                <a class="dropdown-item" href="https://nova.alwaysliquid.com" target="_blank">
+                  Arbitrum Nova <small><i class="bi bi-box-arrow-up-right ms-1"></i></small>
+                </a>
+              </li>
+
+              <li class="pt-1 pb-1" @click="closeLeftSidebar">
                 <a class="dropdown-item" href="https://fantom.alwaysliquid.com" target="_blank">
                   Fantom <small><i class="bi bi-box-arrow-up-right ms-1"></i></small>
                 </a>
